11559a6679f3a650fb0a39d586c9cce97ed7b637
Kazuya Izumi <shikibu9419@gmail.com>
2021-07-03 13:11:58 +0900

    Fix bug

--- main.py
+++ main.py
@@ -44,34 +44,35 @@ async def callback(request: Request, background_tasks: BackgroundTasks):
 async def handle_events(events):
     for event in events:
         message = event.message.text
+        reply_token = event.reply_token
         user_id = event.source.user_id
 
         if re.match(r'レシピを登録', message):
-            await line_api.reply_message(
-                event.reply_token,
+            await line_api.reply_message_async(
+                reply_token,
                 TextMessage(text='URLを登録してね！')
             )
         elif re.match(r'.+で検索$', message):
-            # return searched recipes as carousel
+            # TODO: from mongodb
             columns = [
                 CarouselColumn(
                     title=recipe.name,
                     text='、'.join(recipe.tags),
                     actions=[
-                        URIAction(label='レシピサイトへ', uri=recipe.url),
-                        PostbackAction(label='ping', data=f'id={recipe.id}')
+                        PostbackAction(label='詳細', data=f'id={recipe.id}'),
+                        URIAction(label='レシピサイトへ', uri=recipe.url)
                     ])
              for recipe in recipes]
 
             carousel_template = CarouselTemplate(columns=columns)
             template_message = TemplateSendMessage(alt_text='listed recipes', template=carousel_template)
 
-            line_bot_api.reply_message(event.reply_token, template_message)
+            await line_api.reply_message_async(reply_token, template_message)
         else:
-            await no_match_text(event.reply_token)
+            await no_match_text(reply_token)
 
 def no_match_text(reply_token: str):
-    line_api.reply_message_async(
+    return line_api.reply_message_async(
         reply_token,
         TextMessage(text='もう一度お願いします！')
     )
